b29d08ec9534522ac94f18de19ae0943c316bf7c
Fiooona <845559655@qq.com>
2016-05-20 14:54:37 +0800

    finish

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,13 +1,16 @@
 'use strict';
 function collect_all_even(collection) {
-    var r=[],k=0;
+    var r=[];
+    var k=0;
     for(var i=0;i<collection.length;i++){
-        if((collection[i]%2)==0) {
-            r[k]==collection[i]
+        if(collection[i]%2==0) {
+            r[k]=collection[i] ;
             k++;
         }
+
     }
     return r;
+
 }
 
 module.exports = collect_all_even;
